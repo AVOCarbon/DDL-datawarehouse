74687f96c14a8b6ca7dfc76ac32923d7ed18198a
Andy K <andy.h.kwok@gmail.com>
2018-04-10 12:29:59 +0200

    Update LO-D1_Receptions.sql
    changing "" to '' and ; separator to , separator

--- LO-D1_Receptions.sql
+++ LO-D1_Receptions.sql
@@ -24,23 +24,24 @@ ALTER TABLE dw."LO-D1_Receptions"
 
 -----
 
-  INSERT INTO dw."SYS_Tables_ToLoad" VALUES
-("Poitiers";"dw";"LO-D1_Receptions";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00");
-INSERT INTO dw."SYS_Tables_ToLoad" VALUES
-("Tunisia";"dw";"LO-D1_Receptions";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00");
-INSERT INTO dw."SYS_Tables_ToLoad" VALUES
-("Kunshan";"dw";"LO-D1_Receptions";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00");
-INSERT INTO dw."SYS_Tables_ToLoad" VALUES
-("Tianjin";"dw";"LO-D1_Receptions";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00");
-INSERT INTO dw."SYS_Tables_ToLoad" VALUES
-("Germany";"dw";"LO-D1_Receptions";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00");
-INSERT INTO dw."SYS_Tables_ToLoad" VALUES
-("India";"dw";"LO-D1_Receptions";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00");
-INSERT INTO dw."SYS_Tables_ToLoad" VALUES
-("Korea";"dw";"LO-D1_Receptions";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00");
-INSERT INTO dw."SYS_Tables_ToLoad" VALUES
-("Mexico";"dw";"LO-D1_Receptions";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00";"1900-01-01 00:00:00");
-
+  INSERT INTO dw."SYS_Tables_ToLoad" VALUES 
+ ('Poitiers','dw','LO-D1_Receptions','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00');
+ INSERT INTO dw."SYS_Tables_ToLoad" VALUES 
+ ('Tunisia','dw','LO-D1_Receptions','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00'); 
+ INSERT INTO dw."SYS_Tables_ToLoad" VALUES 
+ ('Kunshan','dw','LO-D1_Receptions','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00'); 
+ INSERT INTO dw."SYS_Tables_ToLoad" VALUES 
+ ('Tianjin','dw','LO-D1_Receptions','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00'); 
+ INSERT INTO dw."SYS_Tables_ToLoad" VALUES 
+ ('Germany','dw','LO-D1_Receptions','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00'); 
+ INSERT INTO dw."SYS_Tables_ToLoad" VALUES 
+ ('India','dw','LO-D1_Receptions','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00'); 
+ INSERT INTO dw."SYS_Tables_ToLoad" VALUES 
+ ('Korea','dw','LO-D1_Receptions','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00'); 
+  INSERT INTO dw."SYS_Tables_ToLoad" VALUES 
+ ('Mexico','dw','LO-D1_Receptions','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00','1900-01-01 00:00:00'); 
+ 
+ 
 
 -----
 
